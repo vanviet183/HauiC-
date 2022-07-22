022688fffb41cf7b2fecf580f02943e2447c80e7
Nguyễn Văn Việt <vanvietgg183@gmail.com>
2022-07-22 23:33:47 +0700

    b6.1

---
+++ Code/Bai 6/b6.1/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b6._1
+{
+    internal class Person
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        private static int cnt = 1;
+
+        public Person(string name, string address)
+        {
+            this.id = cnt++;
+            this.name = name;
+            this.address = address;
+        }
+
+        public Person()
+        {
+            this.id = cnt++;
+
+        }
+
+        public void Input()
+        {
+            Console.Write("Enter the name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter the address: ");
+            address = Console.ReadLine();
+        }
+
+        public void Output()
+        {
+            Console.Write($"{id, 10}" + $"{name, 10}" + $"{address, 10}");
+
+        }
+    }
+}
---
+++ Code/Bai 6/b6.1/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b6._1
+{
+    internal class Student : Person
+    {
+        public byte math { get; set; }
+        public byte physics { get; set; }
+
+        public void Input()
+        {
+            base.Input();
+            Console.Write("Enter the math: ");
+            math = byte.Parse(Console.ReadLine());
+
+            Console.Write("Enter the physics: ");
+            physics = byte.Parse(Console.ReadLine());
+
+        }
+
+        public void Output()
+        {
+            base.Output();
+            Console.WriteLine($"{math,10}" + $"{physics,10}" + $"{Total(), 10}");
+
+        }
+
+        public byte Total()
+        {
+            return (byte) (math + physics);
+        }
+    }
+}
